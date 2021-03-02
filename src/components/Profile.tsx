83e059705041f60bd84d82e09e14dffa39052d9c
Victor <victords895@gmail.com>
2021-03-01 22:24:41 -0300

    theme switcher adicionado

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,30 @@
-import { useContext } from 'react';
-import { ChallengesContext } from '../contexts/ChallengesContext';
-import styles from '../styles/components/Profile.module.css';
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import { MdWbSunny } from "react-icons/md";
+import { FaMoon } from "react-icons/fa";
+import styles from "../styles/components/Profile.module.css";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 export function Profile() {
-    const { level } = useContext(ChallengesContext)
-    return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/victorsantos895.png" alt="Victor Santos"/>
-            <div>
-                <strong> Victor Santos</strong>
-                <p>
-                <img src="icons/level.svg" alt="level"/> 
-                Level {level}
-                </p>
-            </div>
+  const { toggleTheme, currentTheme } = useContext(ThemeContext);
+  const { level } = useContext(ChallengesContext);
+  return (
+    <div className={styles.profileRow}>
+      <div className={styles.profileContainer}>
+        <img src="https://github.com/victorsantos895.png" alt="Victor Santos" />
+        <div>
+          <strong>Victor Santos</strong>
+          <p>
+            <img src="icons/level.svg" alt="Level" />
+            Level {level}
+          </p>
         </div>
-    );
+      </div>
+      {currentTheme === "theme-dark" ? (
+        <MdWbSunny onClick={toggleTheme} />
+      ) : (
+        <FaMoon color="#2E384D" onClick={toggleTheme} />
+      )}
+    </div>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import '../styles/global.css'
 
 import { ChallengesProvider } from '../contexts/ChallengesContext'
-import { CountdownProvider } from '../contexts/CountdownContext'
+import { ThemeProvider } from '../contexts/ThemeContext';
 
 function MyApp({ Component, pageProps }) {
 
 
   return (
+        <ThemeProvider>
         <Component {...pageProps} />
-  )
+        </ThemeProvider>
+  )   
 }
 
 export default MyApp
